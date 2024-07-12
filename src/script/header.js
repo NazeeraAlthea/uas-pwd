ff6f328fb1abf5935a5bda9d30e34ba72d34b348
NazeeraAlthea <ryamaulana2894@gmail.com>
2024-07-12 14:48:30 +0700

    order menu display data mobile

--- src/script/header.js
+++ src/script/header.js
@@ -9,13 +9,15 @@ function initializeHeader() {
                 <a class="text-white" href="">D'Kremes</a>
             </div>
 
-            <i id="menuButton" class="ti ti-align-justified"></i>
-            <div id="navbar" class="wrapper hidden flex-col bg-white fixed top-0 right-0 px-10 py-6 justify-center items-center text-red-950 m-4 shadow rounded gap-5">
-                <i class="ti ti-x absolute right-0 top-0 p-4"></i>
-                <a class="hover:underline" href="index.html">Home</a>
-                <a class="hover:underline" href="orderFoods.html">Order</a>
-                <a class="hover:underline" href="">Contact</a>
-                <a href="login.html" class="bg-red-950 text-white px-6 py-2 rounded border border-red-950 hover:bg-white hover:text-red-950">Login</a>
+            <i id="menuButton" class="ti ti-align-justified sm:hidden"></i>
+            <div id="navbar" class="wrapper sm:p-0 sm:flex sm:relative sm:shadow-none sm:flex-row sm:bg-transparent sm:text-white hidden flex-col bg-white fixed top-0 right-0 px-10 py-6 justify-center items-center text-red-950 m-4 shadow rounded gap-5">
+                <i class="sm:hidden ti ti-x absolute right-0 top-0 p-4"></i>
+                <div class="flex flex-col sm:flex-row gap-5 sm:gap-12 sm:fixed sm:left-1/2 sm:-translate-x-1/2">
+                    <a class="hover:underline" href="index.html">Home</a>
+                    <a class="hover:underline" href="orderFoods.html">Order</a>
+                    <a class="hover:underline" href="">Contact</a>
+                </div>
+                <a href="login.html" class="bg-red-950 sm:hover:text-white sm:hover:bg-red-950 sm:hover:border-white sm:bg-white sm:text-red-950 text-white px-6 py-2 rounded border border-red-950 hover:bg-white hover:text-red-950">Login</a>
             </div>
         </div>
     `;
@@ -60,9 +62,9 @@ function initializeUserHeader() {
                     <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
                     <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
                 </svg>
-                <i id="menuButtonAfter" class="ti ti-dots-vertical text-xl text-white ml-4"></i>
+                <i id="menuButtonAfter" class="ti ti-dots-vertical text-xl text-white ml-4 sm:hidden"></i>
             </div>
-            <div id="navbar" class="wrapper hidden flex-col bg-white text-red-950 m-4 shadow fixed top-0 right-0 px-10 py-6 justify-center items-center rounded gap-5 font-semibold transition-all duration-1000">
+            <div id="navbar" class="wrapper hidden sm:p-0 sm:flex sm:fixed sm:left-1/2 sm:-translate-x-1/2 sm:bg-transparent sm:text-white sm:gap-12 sm:font-normal sm:shadow-none sm:flex-row flex-col bg-white text-red-950 m-4 shadow fixed top-0 right-0 px-10 py-6 justify-center items-center rounded gap-5 font-semibold transition-all duration-1000">
                 <a class="hover:underline" href="index.html">Home</a>
                 <a class="hover:underline" href="orderFoods.html">Order</a>
                 <a class="hover:underline" href="">Contact</a>
--- src/script/order.js
+++ src/script/order.js
@@ -9,21 +9,58 @@ export async function displayItems(type) {
     let card = document.createElement('div');
 
     card.innerHTML = `
-    <div class="flex flex-col border shadow rounded w-60">
-      <img class="w-60 h-40 object-cover p-2" src="assets/pngtree-illustration-of-delicious-spicy-mortar-geprek-chicken-png-image_6564528 2.png" alt="">
+    <div class="flex flex-col border shadow rounded h-72 relative">
+      <img class="object-cover" src="assets/pngtree-illustration-of-delicious-spicy-mortar-geprek-chicken-png-image_6564528 2.png" alt="">
       <div class="flex flex-col items-center">
         <div class="font-semibold text-lg">${e.name}</div>
         <div class="text-red-700 font-bold">${e.price}</div>
       </div>
 
-      <button class="orderButton bg-red-900 text-white p-1 m-2 rounded">Order</button>
+      <button class="orderButton bg-white text-red-950 border border-red-950 hover:bg-red-950 hover:text-white p-1 rounded absolute bottom-0 left-1/2 -translate-x-1/2 mb-4 w-10/12 font-semibold">Order</button>
+
+      <div class="displayProductOrder hidden items-center justify-center gap-4 absolute bottom-0 left-1/2 -translate-x-1/2 mb-4">
+        <button class="reduceOrder flex border px-2 py-1 rounded border-black">-</button>
+        <span class="totalOrder">1</span>
+        <button class="addOrder flex border px-2 py-1 rounded border-black">+</button>
+      </div>
     </div>
     `;
     
     container.appendChild(card);
 
+    let buyOrder = document.getElementById('buyOrder')
     let orderButton = card.querySelector('.orderButton')
-    orderButton.addEventListener('click', () => orderItems(e.price));
+    let displayProductOrder = card.querySelector('.displayProductOrder')
+    let totalOrder = card.querySelector('.totalOrder')
+    let addOrder = card.querySelector('.addOrder')
+    let reduceOrder = card.querySelector('.reduceOrder')
+
+    orderButton.addEventListener('click', () => {
+      orderItems(e.price)
+      buyOrder.classList.remove('hidden')
+      displayProductOrder.classList.remove('hidden')
+      displayProductOrder.classList.add('flex')
+      orderButton.classList.add('hidden')
+    })
+
+    addOrder.addEventListener('click', () => {
+      orderItems(e.price)
+      let x = parseInt(totalOrder.innerHTML) 
+      x++
+      totalOrder.innerHTML = x
+    });
+
+    reduceOrder.addEventListener('click', () => {
+      deleteItems(e.price)
+      let x = parseInt(totalOrder.innerHTML) 
+      x--
+      totalOrder.innerHTML = x
+    });
+
+    if(totalOrder.innerHTML == 0 ) {
+      displayProductOrder.classList.add('hidden')
+      alert(totalOrder.innerHTML)
+    }
   });
 
   function orderItems(price) {
@@ -33,6 +70,15 @@ export async function displayItems(type) {
       let newPrice = parseFloat(price);
       displayPrice.innerText = `Rp ${currentPrice + newPrice}`;
       localStorage.setItem("total price", displayPrice.innerText)
+    }
+  }
+
+  function deleteItems(price) {
+    let displayPrice = document.getElementById('displayPrice')
+    if(displayPrice) {
+      let currentPrice = parseFloat(displayPrice.innerText.replace(/[^0-9.-]+/g,""));
+      let newPrice = parseFloat(price);
+      displayPrice.innerText = `Rp ${currentPrice - newPrice}`;
     }
   }
 } 
