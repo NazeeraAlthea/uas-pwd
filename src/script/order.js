ddf8aefd5597764ea8b662d133f0acefffdfde57
NazeeraAlthea <ryamaulana2894@gmail.com>
2024-07-12 14:51:27 +0700

    bug fix display mobile

--- src/script/order.js
+++ src/script/order.js
@@ -9,16 +9,16 @@ export async function displayItems(type) {
     let card = document.createElement('div');
 
     card.innerHTML = `
-    <div class="flex flex-col border shadow rounded h-72 relative">
+    <div class="flex flex-col border shadow rounded h-72 relative gap-2">
       <img class="object-cover" src="assets/pngtree-illustration-of-delicious-spicy-mortar-geprek-chicken-png-image_6564528 2.png" alt="">
       <div class="flex flex-col items-center">
         <div class="font-semibold text-lg">${e.name}</div>
         <div class="text-red-700 font-bold">${e.price}</div>
       </div>
 
-      <button class="orderButton bg-white text-red-950 border border-red-950 hover:bg-red-950 hover:text-white p-1 rounded absolute bottom-0 left-1/2 -translate-x-1/2 mb-4 w-10/12 font-semibold">Order</button>
+      <button class="orderButton bg-white text-red-950 border border-red-950 hover:bg-red-950 hover:text-white p-1 m-2 rounded font-semibold">Order</button>
 
-      <div class="displayProductOrder hidden items-center justify-center gap-4 absolute bottom-0 left-1/2 -translate-x-1/2 mb-4">
+      <div class="displayProductOrder hidden items-center justify-center gap-4">
         <button class="reduceOrder flex border px-2 py-1 rounded border-black">-</button>
         <span class="totalOrder">1</span>
         <button class="addOrder flex border px-2 py-1 rounded border-black">+</button>
